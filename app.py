ce9bd5babd4d9b58bb97d557d698cdf8d5bd3e0e
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2023-05-03 17:00:36 +0900

    캡스톤9

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, Response
-import cv2
-import datetime
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('logs.html')
 
 
 if __name__ == '__main__':
